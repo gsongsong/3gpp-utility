5e65779b64bc13de7475d28292a947910862416a
Seokseong Jeon <sjeon87@gmail.com>
2018-12-26 02:18:33 +0900

    Add navbar

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
+    <nav-bar></nav-bar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+  import NavBar from './components/NavBar'
+
   export default {
-    name: 'third-gen-electron'
+    name: 'third-gen-electron',
+    components: { NavBar }
   }
 </script>
 
---
+++ src/renderer/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="navbar is-success" role="navigation" aria-label="main navigation">
+    <div id="navbar-main" class="navbar-menu">
+      <div class="navbar-start">
+        <router-link to="/" class="navbar-item">Home</router-link>
+        <router-link to="/formatter" class="navbar-item">Formatter</router-link>
+        <router-link to="/idc" class="navbar-item">HM/IMD</router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+</style>
