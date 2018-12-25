8cccee8bcc8df57fd8152efe14bec2fdb217b48d
Seokseong Jeon <sjeon87@gmail.com>
2018-12-25 22:01:44 +0900

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: '3gpp-electron'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
