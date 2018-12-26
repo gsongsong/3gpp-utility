57309d1d81318532af31309533c678f9fc995efd
Seokseong Jeon <sjeon87@gmail.com>
2018-12-26 14:33:00 +0900

    Add navbar burger (because navbar main disappears if width is small) and navbar brand

--- src/renderer/components/NavBar.vue
+++ src/renderer/components/NavBar.vue
@@ -1,5 +1,14 @@
 <template>
   <nav class="navbar is-success" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <router-link to="/" class="navbar-item">3GPP</router-link>
+      <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"
+         data-target="navbar-main">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
     <div id="navbar-main" class="navbar-menu">
       <div class="navbar-start">
         <router-link to="/" class="navbar-item">Home</router-link>
@@ -11,6 +20,27 @@
 </template>
 
 <script>
+  document.addEventListener('DOMContentLoaded', () => {
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0)
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+      // Add a click event on each of them
+      $navbarBurgers.forEach(el => {
+        el.addEventListener('click', () => {
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target
+          const $target = document.getElementById(target)
+
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active')
+          $target.classList.toggle('is-active')
+        })
+      })
+    }
+  })
+
   export default {
   }
 </script>
