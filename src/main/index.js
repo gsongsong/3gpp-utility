b3b0db7adb3cb932a6462b79f713b44a1b63c30e
Seokseong Jeon <sjeon87@gmail.com>
2018-12-28 22:31:48 +0900

    Adjust default window size

--- src/main/index.js
+++ src/main/index.js
@@ -21,9 +21,9 @@ function createWindow () {
    * Initial window options
    */
   mainWindow = new BrowserWindow({
-    height: 563,
+    height: 600,
     useContentSize: true,
-    width: 1000
+    width: 1200
   })
 
   workerWindow = new BrowserWindow({show: false})
