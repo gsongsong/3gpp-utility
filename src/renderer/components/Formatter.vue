1532167dcfacea6d0d6ba7f0aeec4cfbd0b87767
Seokseong Jeon <sjeon87@gmail.com>
2018-12-28 22:23:53 +0900

    Complete working prototype of formatter

--- src/renderer/components/Formatter.vue
+++ src/renderer/components/Formatter.vue
@@ -14,31 +14,40 @@
         </b-tag>
       </span>
     </b-field>
-    <div v-if="specType !== 'Unknown'">
+    <div>
       <b-field class="control" grouped>
-        <b-input v-model="msgIeName" placeholder="Message/IE name"></b-input>
-        <b-checkbox v-model="doNotExpand" native-value="true" type="is-success">Do not expand sub-IE</b-checkbox>
+        <b-input v-model="msgIeName" :disabled="specType === 'Unknown'" placeholder="Message/IE name"></b-input>
+        <b-checkbox v-model="doNotExpand" native-value="true" :disabled="specType === 'Unknown'" type="is-success">Do not expand sub-IE</b-checkbox>
       </b-field>
       <b-field grouped>
         <p class="control">
-          <button class="button is-success" :disabled="!msgIeName">Format message/IE</button>
+          <button class="button is-success" :disabled="specType === 'Unknown' || !msgIeName" v-on:click="format(msgIeName)">Format message/IE</button>
         </p>
         <p class="control">
-          <button class="button is-success" v-if="specType === 'Application Protocol'">Format all</button>
+          <b-tooltip label="Application Protocol only" position="is-right" type="is-info">
+            <button class="button is-success" :disabled="specType !== 'Application Protocol'" v-on:click="format('__all')">Format all</button>
+          </b-tooltip>
         </p>
       </b-field>
+      <b-loading :active.sync="isFormatting" :is-full-page="true"></b-loading>
     </div>
   </div>
 </template>
 
 <script>
+  import { parse } from 'path'
+  import { ipcRenderer, remote, shell } from 'electron'
+  import * as xlsx from '@gsongsong/xlsx'
+
   export default {
     data () {
       return {
         file: null,
         specType: 'Unknown',
         msgIeName: null,
-        doNotExpand: []
+        doNotExpand: [],
+        isFormatting: false,
+        defaultPathDir: null
       }
     },
     methods: {
@@ -60,7 +69,68 @@
         if (indexAp >= 0 && (indexRrc === -1 || (indexRrc >= 0 && indexAp < indexRrc))) {
           this.specType = 'Application Protocol'
         }
+      },
+      format: function (msgIeName) {
+        this.isFormatting = true
+        if (msgIeName === '__all') {
+          this.msgIeName = null
+        }
+        let data = {
+          filePath: this.file.path,
+          specType: this.specType,
+          msgIeName: msgIeName,
+          raw: !!this.doNotExpand.length
+        }
+        ipcRenderer.send('format-request', JSON.stringify(data))
       }
+    },
+    mounted () {
+      ipcRenderer.on('format-response', (event, data) => {
+        let result = JSON.parse(data)
+        if (result.error) {
+          this.$snackbar.open({
+            message: result.error,
+            type: 'is-warning',
+            position: 'is-bottom-right',
+            actionText: 'Dismiss',
+            indefinite: true,
+            queue: false
+          })
+        } else {
+          let pathParsed = parse(this.file.path)
+          let defaultPathDir = this.defaultPathDir ? this.defaultPathDir : pathParsed.dir
+          let msgIeName = this.msgIeName ? `${this.msgIeName}-` : ''
+          let raw = this.doNotExpand.length ? '-raw' : ''
+          let savePath = remote.dialog.showSaveDialog({
+            defaultPath: `${defaultPathDir}/${msgIeName}${pathParsed.name}${raw}.xlsx`
+          })
+          if (savePath) {
+            this.defaultPathDir = parse(savePath).dir
+            xlsx.writeFile(result, savePath)
+            this.$snackbar.open({
+              message: 'Formatting success',
+              type: 'is-success',
+              position: 'is-bottom-right',
+              actionText: 'Open folder',
+              duration: 10 * 1000,
+              queue: false,
+              onAction: () => {
+                shell.openExternal(this.defaultPathDir)
+              }
+            })
+          } else {
+            this.$snackbar.open({
+              message: 'Save aborted',
+              type: 'is-warning',
+              position: 'is-bottom-right',
+              actionText: 'Dismiss',
+              duration: 3 * 1000,
+              queue: false
+            })
+          }
+        }
+        this.isFormatting = false
+      })
     }
   }
 </script>
