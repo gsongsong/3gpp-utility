d0275e46c6986c010e162b377d23c6b96e402002
Seokseong Jeon <sjeon87@gmail.com>
2018-12-31 01:39:47 +0900

    Save as XLSX prototype

--- src/renderer/components/Idc.vue
+++ src/renderer/components/Idc.vue
@@ -2,19 +2,19 @@
   <div id="wrapper" class="section">
     <div class="columns">
       <div class="column is-one-quarter">
-        <frequency-table heading="RAT 1 Downlink" v-on:data-changed="onDataChange($event, 'rat1Dl')">
+        <frequency-table :heading="rat1DlHeading" v-on:data-changed="onDataChange($event, 'rat1Dl')">
         </frequency-table>
       </div>
       <div class="column is-one-quarter">
-        <frequency-table heading="RAT 1 Uplink" v-on:data-changed="onDataChange($event, 'rat1Ul')">
+        <frequency-table :heading="rat1UlHeading" v-on:data-changed="onDataChange($event, 'rat1Ul')">
         </frequency-table>
       </div>
       <div class="column is-one-quarter">
-        <frequency-table heading="RAT 2 Downlink" v-on:data-changed="onDataChange($event, 'rat2Dl')">
+        <frequency-table :heading="rat2DlHeading" v-on:data-changed="onDataChange($event, 'rat2Dl')">
         </frequency-table>
       </div>
       <div class="column is-one-quarter">
-        <frequency-table heading="RAT 2 Uplink" v-on:data-changed="onDataChange($event, 'rat2Ul')">
+        <frequency-table :heading="rat2UlHeading" v-on:data-changed="onDataChange($event, 'rat2Ul')">
         </frequency-table>
       </div>
     </div>
@@ -59,7 +59,7 @@
       </div>
     </div>
     <div class="columns is-centered">
-      <button class="button is-success">
+      <button class="button is-success" v-on:click="save">
         Save
       </button>
     </div>
@@ -67,7 +67,9 @@
 </template>
 
 <script>
-  import { ipcRenderer } from 'electron'
+  import { parse } from 'path'
+  import { ipcRenderer, remote } from 'electron'
+  import * as xl from 'excel4node'
 
   import FrequencyTable from './FrequencyTable'
   import IdcTable from './IdcTable'
@@ -76,6 +78,10 @@
     components: { FrequencyTable, IdcTable },
     data () {
       return {
+        rat1DlHeading: 'RAT 1 Downlink',
+        rat1UlHeading: 'RAT 1 Uplink',
+        rat2DlHeading: 'RAT 2 Downlink',
+        rat2UlHeading: 'RAT 2 Uplink',
         rat1Dl: [],
         rat1Ul: [],
         rat2Dl: [],
@@ -116,6 +122,66 @@
       },
       onDataChange: function (data, freqTableName) {
         this[freqTableName] = data
+      },
+      insertFreqTable (freqs, heading, ws, rowStart, colStart) {
+        let row = rowStart
+        let col = colStart
+        ws.cell(row, col).string(heading)
+        row++
+        ws.cell(row, col).string('Name')
+        ws.cell(row, col + 1).string('Freq Low')
+        ws.cell(row, col + 2).string('Freq High')
+        for (let freq of freqs) {
+          row++
+          ws.cell(row, col).string(freq.name)
+          ws.cell(row, col + 1).number(freq.fLow)
+          ws.cell(row, col + 2).number(freq.fHigh)
+        }
+      },
+      insertIdcFreqTable (freqs, heading, ws, rowStart, colStart) {
+        let row = rowStart
+        let col = colStart
+        ws.cell(row, col).string(heading)
+        row++
+        ws.cell(row, col).string('Order')
+        ws.cell(row, col + 1).string('Name')
+        ws.cell(row, col + 2).string('Freq Low')
+        ws.cell(row, col + 3).string('Freq High')
+        ws.cell(row, col + 4).string('Victim')
+        for (let freq of freqs) {
+          row++
+          ws.cell(row, col).number(freq.order)
+          ws.cell(row, col + 1).string(freq.name)
+          ws.cell(row, col + 2).number(freq.fLow)
+          ws.cell(row, col + 3).number(freq.fHigh)
+          ws.cell(row, col + 4).string(freq.victim)
+        }
+      },
+      save: function () {
+        if (!this.bandsHarmonics.length && !this.bandsImd.length) {
+          return
+        }
+        this.isWorking = true
+        let wb = new xl.Workbook()
+        let ws = wb.addWorksheet('Sheet 1')
+        this.insertFreqTable(this.rat1Dl, this.rat1DlHeading, ws, 1, 2)
+        this.insertFreqTable(this.rat1Ul, this.rat1UlHeading, ws, 1, 6)
+        this.insertFreqTable(this.rat2Dl, this.rat2DlHeading, ws, 1, 10)
+        this.insertFreqTable(this.rat2Ul, this.rat2UlHeading, ws, 1, 14)
+        let maxNumFreqs = Math.max(this.rat1Dl.length, this.rat1Ul.length, this.rat2Dl.length, this.rat2Ul.length)
+        this.insertIdcFreqTable(this.bandsHarmonics, 'Harmonic Interference', ws, maxNumFreqs + 4, 1)
+        this.insertIdcFreqTable(this.bandsImd, 'IMD Interference', ws, maxNumFreqs + 4, 9)
+        let savePath = remote.dialog.showSaveDialog({
+          defaultPath: this.defaultPath ? this.defaultPath : null,
+          filters: [
+            {name: 'XLSX', extensions: ['xlsx']}
+          ]
+        })
+        if (savePath) {
+          this.defaultPath = parse(savePath).dir
+          wb.write(savePath)
+        }
+        this.isWorking = false
       }
     },
     mounted () {
