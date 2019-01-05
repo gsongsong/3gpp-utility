22cabb0a4312a389c50e3852b5010ea33c8b1ce4
Seokseong Jeon <sjeon87@gmail.com>
2019-01-06 02:05:52 +0900

    Spec watchdog skeleton

--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -1,13 +1,33 @@
 <template>
-  <div id="wrapper">
+  <div id="specWatchDogWrapper" class="section columns">
+    <div v-for="item in Object.keys(items)" v-bind:key="item" class="column is-one-quarter">
+      <spec-table :heading="item" :block="items[item]"></spec-table>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'home'
+  import SpecTable from './SpecTable'
+
+export default {
+    name: 'home',
+    components: { SpecTable },
+    data () {
+      return {
+        items: {
+          '36.331': [
+            {name: '36331-f00.zip', type: '-', size: 0, date: '0000-00-00', url: 'http'}
+          ],
+          '': []
+        }
+      }
+    }
   }
 </script>
 
 <style>
+  #specWatchDogWrapper {
+    display: flex;
+    flex-wrap: wrap;
+  }
 </style>
---
+++ src/renderer/components/SpecTable.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="panel">
+    <div class="panel-heading">
+      <span v-if="heading">
+        {{ heading }}
+      </span>
+      <b-field v-if="!heading">
+        <b-input placeholder="Spec number"></b-input>
+        <p class="control">
+          <button class="button is-success">✚</button>
+        </p>
+      </b-field>
+    </div>
+    <div class="panel-block">
+      <b-table :data="block" v-if="heading" striped narrowed hoverable>
+        <template slot-scope="props">
+          <b-table-column field="fileName" label="File name">
+            {{ props.row.name }}
+          </b-table-column>
+          <b-table-column field="fileName" label="Date">
+            {{ props.row.date }}
+          </b-table-column>
+        </template>
+      </b-table>
+      <span v-if="!heading">
+        Add spec number of interest
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      heading: {
+        type: String,
+        default: ''
+      },
+      block: {
+        type: Array,
+        default: []
+      }
+    },
+    data () {
+      return {
+      }
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style>
+</style>
