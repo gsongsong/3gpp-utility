99cdf8e239fb9d578ac46042112c523910b83961
Jeon <sjeon87+gitkraken@gmail.com>
2019-01-14 00:19:41 +0900

    Add version and version checking

--- src/renderer/components/NavBar.vue
+++ src/renderer/components/NavBar.vue
@@ -22,10 +22,23 @@
         <router-link to="/idc" class="navbar-item">HM/IMD</router-link>
       </div>
     </div>
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <a class="button is-info is-small" v-if="newVersionAvailable" @click="openRelease">
+          New version available!
+        </a>
+      </div>
+    </div>
   </nav>
 </template>
 
 <script>
+  import * as request from 'request'
+  import { parse } from 'url'
+  import * as urlJoin from 'url-join'
+  import { remote, shell } from 'electron'
+  import { repository } from '../../../package.json'
+
   document.addEventListener('DOMContentLoaded', () => {
     // Get all "navbar-burger" elements
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0)
@@ -48,6 +61,49 @@
   })
 
   export default {
+    data () {
+      return {
+        version: remote.app.getVersion(),
+        newVersionAvailable: false
+      }
+    },
+    methods: {
+      openRelease () {
+        shell.openExternal(urlJoin(repository.url, 'releases/latest'))
+      }
+    },
+    mounted () {
+      const urlObj = parse(repository.url)
+      const apiReleasesUrl = urlJoin(urlObj.protocol, 'api.github.com', 'repos', urlObj.path, 'releases/latest')
+      const headers = {
+        'User-Agent': 'gsongsong/3gpp-electron',
+        'Authorization': 'token 5c68b07d9bd331aef636501c0ff8172a495a30d6'
+      }
+      request({url: apiReleasesUrl, headers: headers},
+        (e, res, body) => {
+          let json = {}
+          try {
+            json = JSON.parse(body)
+          } catch (e) {
+            return
+          }
+          if (!('tag_name' in json)) {
+            return
+          }
+          const tagName = json.tag_name
+          const last = tagName.substring(1).split('.').map(el => Number(el))
+          const curr = this.version.split('.').map(el => Number(el))
+          for (let i = 0; i < last.length; i++) {
+            if (last[i] > curr[i]) {
+              this.newVersionAvailable = true
+            } else if (last[i] === curr[i]) {
+              continue
+            } else {
+              break
+            }
+          }
+        })
+    }
   }
 </script>
 
